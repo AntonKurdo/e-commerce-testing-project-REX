3cbdb677f41cebf330e3308e2905dbf52fca1314
Anton Kurdo <anton.kurdo@gmail.com>
2020-08-14 14:13:30 +0300

    Updates

---
+++ precache-manifest.0be2dd77f8f67e86d6a13c0767857d03.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c81bb5b03b9835ccc4b64da02902829",
+    "url": "/e-commerce-testing-project-REX/index.html"
+  },
+  {
+    "revision": "3ac1ab613d35bfacc28c",
+    "url": "/e-commerce-testing-project-REX/static/css/main.251f9b0b.chunk.css"
+  },
+  {
+    "revision": "e1458a43623c5fbd1082",
+    "url": "/e-commerce-testing-project-REX/static/js/2.12889171.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/e-commerce-testing-project-REX/static/js/2.12889171.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ac1ab613d35bfacc28c",
+    "url": "/e-commerce-testing-project-REX/static/js/main.f456ae54.chunk.js"
+  },
+  {
+    "revision": "12ad84a018991a22da9b",
+    "url": "/e-commerce-testing-project-REX/static/js/runtime-main.f675c28a.js"
+  },
+  {
+    "revision": "24020fdb7381685e4f5c644f00b199cc",
+    "url": "/e-commerce-testing-project-REX/static/media/10.24020fdb.png"
+  },
+  {
+    "revision": "fc38f2cf511d4df3f6c79cd61e70c1cb",
+    "url": "/e-commerce-testing-project-REX/static/media/3.fc38f2cf.png"
+  },
+  {
+    "revision": "d06dca6732febbe9b23d65dd7cbd854c",
+    "url": "/e-commerce-testing-project-REX/static/media/4.d06dca67.png"
+  },
+  {
+    "revision": "05476869f745f27112a1bd098c2def04",
+    "url": "/e-commerce-testing-project-REX/static/media/5.05476869.png"
+  },
+  {
+    "revision": "ed8808d5c9c5223f892169c07927048d",
+    "url": "/e-commerce-testing-project-REX/static/media/6.ed8808d5.png"
+  },
+  {
+    "revision": "174bc9fc66718c839226c74ececda8fb",
+    "url": "/e-commerce-testing-project-REX/static/media/7.174bc9fc.png"
+  },
+  {
+    "revision": "c0143d13cea559e252a3ebde357d5aa4",
+    "url": "/e-commerce-testing-project-REX/static/media/8.c0143d13.png"
+  },
+  {
+    "revision": "1ae726e29d34d4a3d7cb4e8d9d7016f0",
+    "url": "/e-commerce-testing-project-REX/static/media/9.1ae726e2.png"
+  },
+  {
+    "revision": "a7125a9e8c4ee1091d10c37da67838f6",
+    "url": "/e-commerce-testing-project-REX/static/media/banner.a7125a9e.png"
+  }
+]);
